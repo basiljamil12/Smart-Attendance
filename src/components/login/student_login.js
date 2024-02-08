e5a6e42da7b89400b50c13557eb48d351b9da554
Basil <basiljamil1122@gmail.com>
2024-02-09 01:53:58 +0500

    Pushing

--- src/components/login/student_login.js
+++ src/components/login/student_login.js
@@ -22,9 +22,9 @@ function StudentLogin() {
     };
     return (
         <div className="flex flex-col min-h-[100vh]">
-            {/* <div>
+            <div>
                 <PlainNavbar />
-            </div> */}
+            </div>
             <div className="flex flex-col flex-grow justify-center items-center mt-10">
                 <div className="text-sa-maroon text-[40px] font-bold mb-14">STUDENT LOGIN</div>
                 <div className={"relative mb-6 text-black"}>
@@ -62,7 +62,7 @@ function StudentLogin() {
                 <div class="relative mb-4 ">
                 <button 
                 //className="mb-4 h-[45px] md:h-[56px] bg-sa-maroon rounded-[20px] md:w-[102%] w-[245px] md:mr-3  shadow-md mx-5 text-white font-bold text-[26px]"
-                class="transition-opacity hover:opacity-90 font-bold shadow-xl focus:outline-none focus:ring-0 bg-sa-maroon  focus:border-clue-purchase peer m-0 block h-[45px] md:h-[56px]   md:mr-72  md:w-[103%] w-[245px]  rounded-[20px]   bg-clip-padding px-3 py-2 text-base  leading-tight text-white text-[26px]"
+                class="transition-opacity hover:opacity-90 font-bold shadow-xl focus:outline-none focus:ring-0 bg-sa-maroon  focus:border-clue-purchase peer m-0 block h-[45px] md:h-[56px]   md:mr-72  md:w-[103%] w-[245px]  rounded-[20px]   bg-clip-padding px-3 py-2 text-base  leading-tight text-white text-[24px]"
                 
                 >
                    
@@ -92,9 +92,9 @@ function StudentLogin() {
                      </div> */}
 
             </div>
-            {/* <div className="mt-10">
+            <div className="mt-10">
                 <Footer />
-            </div> */}
+            </div>
         </div>
     );
 }
