411c927284ca5880d3b17014d80dceba9d5a76f5
Diyan Ali Shaikh <shaikhdiyanali01@gmail.com>
2024-02-21 21:04:40 +0500

    edit faculty

--- src/pages/admin_pages/faculty/edit_faculty.js
+++ src/pages/admin_pages/faculty/edit_faculty.js
@@ -36,7 +36,6 @@ function EditFaculty() {
           setfacultyName(value.data.name);
           setContactno(value.data.contactno);
           setEmail(value.data.email);
-          setPassword(value.data.password);
           setIsAdvisor(value.data.isStudentAdvisor.toString());
           setShowLoading(false);
         } else {
@@ -122,17 +121,17 @@ function EditFaculty() {
       ]);
       return;
     }
-    if (!password.trim()) {
-      setToastMessages([
-        ...toastMessages,
-        {
-          type: "invalid",
-          title: "Invalid Password",
-          body: "Password cannot be empty",
-        },
-      ]);
-      return;
-    }
+    // if (!password.trim()) {
+    //   setToastMessages([
+    //     ...toastMessages,
+    //     {
+    //       type: "invalid",
+    //       title: "Invalid Password",
+    //       body: "Password cannot be empty",
+    //     },
+    //   ]);
+    //   return;
+    // }
     if (!contactno) {
       setToastMessages([
         ...toastMessages,
@@ -200,14 +199,11 @@ function EditFaculty() {
     }
     // Perform create action, navigate, etc.
   };
+
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
-  // const studentEmailOptions = [
-  //   { value: "student1@example.com", label: "Student 1" },
-  //   { value: "student2@example.com", label: "Student 2" },
-  //   { value: "student3@example.com", label: "Student 3" },
-  // ];
+
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
   useEffect(() => {
@@ -221,6 +217,67 @@ function EditFaculty() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  const [passwordShowLoading, setPasswordShowLoading] = useState(false);
+  const [passwordIdx, setPasswordIdx] = useState(0);
+  const [isPassword, setIsPassword] = useState("");
+
+  const closeIsPassword = () => {
+    setIsPassword(false);
+  };
+  const openIsPassword = (id) => {
+    setPasswordIdx(id);
+    setIsPassword(true);
+  };
+
+  const handlePasswordUpdate = async () => {
+    if (!password.trim()) {
+      setToastMessages([
+        ...toastMessages,
+        {
+          type: "invalid",
+          title: "Invalid Password",
+          body: "Password cannot be empty",
+        },
+      ]);
+      return;
+    }
+    try {
+      setPasswordShowLoading(true);
+      const response = await editFacultyManager.editPassword(id, password);
+      if (response.success) {
+        setToastMessages([
+          ...toastMessages,
+          {
+            type: "success",
+            title: "Success",
+            body: response.message,
+          },
+        ]);
+        closeIsPassword();
+      } else {
+        setToastMessages([
+          ...toastMessages,
+          {
+            type: "invalid",
+            title: "Error",
+            body: response.message,
+          },
+        ]);
+      }
+    } catch (response) {
+      setToastMessages([
+        ...toastMessages,
+        {
+          type: "invalid",
+          title: "Error",
+          body: response.message,
+        },
+      ]);
+    } finally {
+      setPasswordShowLoading(false);
+    }
+  };
 
   return (
     <div className="flex">
@@ -237,12 +294,12 @@ function EditFaculty() {
           }}
         />
       ))}
-      <div className="md:ml-14 ml-5 w-full mx-[6%] ">
+      <div className="w-full mx-[6%]">
         <p className="text-sa-maroon text-[32px] md:text-[36px]  mb-5 text-left md:mt-14 mt-20 font-bold">
           Edit Faculty
         </p>
 
-        <div className="md:ml-3 ml-2 mt-5 md:mt-6 mb-5 ">
+        <div className="mt-5 md:mt-6 mb-5 ">
           <input
             type="text"
             id="facultyName"
@@ -252,7 +309,7 @@ function EditFaculty() {
             className="placeholder-gray-500 w-full h-14 md:h-16  border-[1px] border-black border-solid   text-black p-2 rounded-xl focus:outline-none focus:ring-0 focus:border focus:border-sa-maroon"
           />
         </div>
-        <div className="md:ml-3 ml-2 mt-5 md:mt-6 mb-5 ">
+        <div className="mt-5 md:mt-6 mb-5 ">
           <input
             type="email"
             id="email"
@@ -262,7 +319,7 @@ function EditFaculty() {
             className="placeholder-gray-500 w-full h-14 md:h-16 border-[1px] border-black border-solid   text-black p-2 rounded-xl focus:outline-none focus:ring-0 focus:border focus:border-sa-maroon"
           />
         </div>
-        <div class="relative  md:ml-3 ml-2 mt-5 md:mt-6 mb-5  ">
+        {/* <div class="relative  md:ml-3 ml-2 mt-5 md:mt-6 mb-5  ">
           <input
             type={showPassword ? "text" : "password"}
             //class="shadow-xl focus:outline-none focus:ring-0 focus:border-sa-maroon peer m-0 block h-[45px] md:h-[56px]   md:mr-44  md:w-[102%] w-[245px]  rounded-[20px] border-[1px] border-solid  border-black  bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-black"
@@ -283,9 +340,9 @@ function EditFaculty() {
               onClick={toggleShowPassword}
             />
           )}
-        </div>
+        </div> */}
 
-        <div className="md:ml-3 ml-2 mt-5 md:mt-6 mb-5 ">
+        <div className="mt-5 md:mt-6 mb-5 ">
           <input
             type="number"
             id="contactNo"
@@ -296,7 +353,7 @@ function EditFaculty() {
           />
         </div>
 
-        <div className="md:ml-3 ml-2 mt-5 md:mt-6 mb-10 ">
+        <div className="mt-5 md:mt-6 mb-10 ">
           <select
             id="isAdvisor"
             onChange={handleIsAdvisorChange}
@@ -313,12 +370,61 @@ function EditFaculty() {
 
         <div class="flex items-center justify-center mb-14   ">
           <button
-            class=" transition-opacity hover:opacity-90 font-bold shadow-xl focus:outline-none focus:ring-0 bg-sa-maroon  focus:border-sa-maroon peer m-0 block h-[55px] md:h-[56px]  md:w-[250px] w-[220px]  rounded-[20px]   bg-clip-padding px-3 md:py-2   leading-tight text-white text-[20px] md:text-[24px]"
+            class=" transition-opacity hover:opacity-90 font-bold shadow-xl focus:outline-none focus:ring-0 bg-sa-maroon  focus:border-sa-maroon peer m-0 block h-[40px] md:h-[56px]  md:w-[250px] w-[200px]  rounded-[20px]   bg-clip-padding px-3 md:py-2   leading-tight text-white text-[14px] md:text-[24px]"
             onClick={handleEdit}
           >
             {showLoading ? <Spinner /> : "Save"}
           </button>
+          <button
+            class="md:ml-5 transition-opacity hover:opacity-90 font-bold shadow-xl focus:outline-none focus:ring-0 bg-sa-maroon  focus:border-sa-maroon peer m-0 block h-[40px] md:h-[56px]  md:w-[250px] w-[200px]  rounded-[20px]   bg-clip-padding px-3 md:py-2   leading-tight text-white text-[14px] md:text-[24px]"
+            onClick={openIsPassword}
+          >
+            Change Password
+          </button>
         </div>
+        {isPassword && (
+          <div
+            className=" fixed inset-0 flex items-center justify-center z-50"
+            onClick={closeIsPassword}
+          >
+            <div className=" bg-black opacity-50 absolute inset-0"></div>
+            <div
+              className=" bg-white rounded-3xl md:w-[30%] w-80  p-8 px-12 relative z-10"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <h2 className="text-black font-semibold md:w-auto w-60 text-left mb-4">
+                Update
+              </h2>
+              <p className="text-black text-filter-heading md:w-auto w-60 text-left">
+                Enter the new password here:
+              </p>
+              <div className="mt-12 md:mt-8 mb-5 ">
+                <input
+                  type="text"
+                  id="newpass"
+                  placeholder="Enter New Password"
+                  onChange={handlePasswordChange}
+                  value={password}
+                  className="placeholder-gray-500 w-full flex items-start justify-start  h-14 md:h-16 py-4  border-[1px] border-black border-solid   text-black p-2 rounded-xl focus:outline-none focus:ring-0 focus:border focus:border-sa-maroon"
+                />
+              </div>
+              <div className="flex justify-end mt-6">
+                <button
+                  onClick={closeIsPassword}
+                  className="text-filter-heading transition-opacity hover:opacity-70 mr-4 border-2 border-gray-400 rounded-[9px] border-filter-heading py-1 px-6"
+                >
+                  Cancel
+                </button>
+                <button
+                  className="bg-sa-maroon transition-opacity hover:opacity-70 text-white md:px-7 px-5 rounded-[9px] py-1 "
+                  onClick={handlePasswordUpdate}
+                >
+                  {passwordShowLoading ? <Spinner /> : <span>Save</span>}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
